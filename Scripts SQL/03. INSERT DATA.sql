401c49e4eea5d108c4200f98476e55362bf9be73
Joel Rojas <joelrojas_rm@hotmail.com>
2023-07-14 09:20:30 -0500

    Scripts

---
+++ Scripts SQL/03. INSERT DATA.sql
@@ -0,0 +1,19 @@
+
+USE [DBPruebaPromart]
+GO
+
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'Joel', N'Rojas Maldonado', CAST(N'1992-02-26T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'Renzo', N'Perez Soza', CAST(N'1990-10-05T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'Cesar', N'Vilca Mendoza', CAST(N'1995-12-12T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'Yomira', N'Vargas Mendoza', CAST(N'1989-10-02T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'Alana', N'Rojas Vargas', CAST(N'2018-09-19T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'José', N'Pérez Reynoso', CAST(N'1998-10-02T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Client] ( [Name], [LastName], [BirthDate]) VALUES ( N'sss', N'sss', CAST(N'2023-07-14T00:00:00.000' AS DateTime))
+GO
+
